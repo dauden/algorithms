a3195a52fb763cd12512485f2804aaaa4ed2b796
dsln <sav.nardi@gmail.com>
2018-05-11 21:19:35 -0400

    Complete first pass of maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let dict = new Map();
+    let maxVal = '';
+    let currentMax = 0;
+    for (const char of str) {
+        let val = 1;
+        if (dict.has(char)) {
+            val = dict.get(char);
+            dict.set(char, ++val);
+        } else {
+            dict.set(char, val);
+        }
+        if (val > currentMax) {
+            currentMax = dict.get(char);
+            maxVal = char;
+        }
+    } 
+    return maxVal;
+}
 
 module.exports = maxChar;
